c729a4cceb966e38f35f898938a0683688264ef4
jtan <jtan@MTP-D3020-ROU006.DIGINAMIC.LOCAL>
2018-01-12 14:45:06 +0100

    routes + init files

--- WebContent/app/js/media/media.js
+++ WebContent/app/js/media/media.js
@@ -1,1 +1,10 @@
-angular.module('media', ['tomatoMedia']);
+angular.module('media', ['tomatoMedia', 'ngRoute'].config(function($routeProvider) {
+	$routeProvider
+		.when('/media-search', {
+			templateUrl: './html/search.html',
+			controller: 'searchCtrl'
+		})
+		.otherwise({
+			redirectTo: '/media-search'
+		})
+}));
