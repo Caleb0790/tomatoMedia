51cd39d519a56da52e84913c0f847ebac7e724f7
jtan <jtan@MTP-D3020-ROU006.DIGINAMIC.LOCAL>
2018-01-12 15:19:34 +0100

    index.html:  -> <script> vers fichiers; search.html:  -> structure tableau + barre de recherche; searchCtrl.js:  -> appel au mediaService.

--- WebContent/app/js/media/controller/searchCtrl.js
+++ WebContent/app/js/media/controller/searchCtrl.js
@@ -1,3 +1,10 @@
 /**
- * 
+ * Controleur pour la recherche
  */
+angular.module('media').controller('searchCtrl', ['$scope', 'mediaService', function($scope, mediaService) {
+	$scope.list = [];
+	mediaService.getAll().then(function(mediaList) {
+		$scope.list = mediaList;
+		console.log('List loaded !');
+	});
+}])
