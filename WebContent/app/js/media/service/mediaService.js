618b539599b4cb0063f9f71748441207a19978fb
cjoseph <cjoseph@MTP-D3020-ROU008.DIGINAMIC.LOCAL>
2018-01-12 16:09:02 +0100

    add getOne function

--- WebContent/app/js/media/service/mediaService.js
+++ WebContent/app/js/media/service/mediaService.js
@@ -3,6 +3,7 @@ angular.module('tomatoMedia').service('mediaService', ['$http', function($http){
 
 	this.getAll = function(){
 		return mediaPromise.then( function(response){
+			console.log('getAll()');
 			return response;
 		})
 	}
@@ -10,6 +11,11 @@ angular.module('tomatoMedia').service('mediaService', ['$http', function($http){
 	var alreadyLoadedMedia = {} ;
 	
 	this.getOne = function(mediaId){
-		
+		if(!alreadyLoadedMedia[mediaId]){
+			alreadyLoadedMedia[mediaId] = $http.get('http://192.168.1.16:8090/resource/media.accession/'+mediaId);
+		}
+		return alreadyLoadedMedia[mediaId].then(function(response){
+			return response;
+		})
 	}
 }])
