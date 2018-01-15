d9edb1fa00e25af8ce8a946602321ea6a57c90e7
Caleb JOSEPH <cjoseph@DIGINAMIC.LOCAL>
2018-01-15 12:45:20 +0100

    refactoring : centralisation du controller.

--- WebContent/app/js/media/media.js
+++ WebContent/app/js/media/media.js
@@ -6,19 +6,42 @@ angular.module('media', ['ngRoute']).config(function($routeProvider) {
 		})
 		.when('/media-search', {
 			templateUrl: './html/search.html',
-			controller: 'searchCtrl'
+			controller: 'searchCtrl',
+			resolve: {
+				media: function(mediaService){
+					return mediaService.getAll();
+				}
+			}
 		})
 		.when('/media-search/searchDVD', {
 			templateUrl: './html/search.html',
-			controller: 'searchDVDCtrl'
+			controller: 'searchCtrl',
+			resolve: {
+				media: function(mediaService){
+					return mediaService.getDvds();
+				}
+			}
 		})
 		.when('/media-search/searchBook', {
 			templateUrl: './html/search.html',
-			controller: 'searchBookCtrl'
+			controller: 'searchCtrl',
+			resolve: {
+				media: function(mediaService){
+					return mediaService.getBooks();
+				}
+			}
 		})
 		.when('/media-search/searchCD', {
 			templateUrl: './html/search.html',
-			controller: 'searchCDCtrl'
+			controller: 'searchCtrl',
+			resolve: {
+				media: function(mediaService){
+					return mediaService.getCds();
+				}
+			}
+		})
+		.when('/edit/:id', {
+			templateUrl: './html/modif.html'
 		})
 		.otherwise({
 			redirectTo: '/media-search'
