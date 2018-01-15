986f5d5c31c13fd7666107b2c1342394b9b0ccd4
Julie TAN <jtan@DIGINAMIC.LOCAL>
2018-01-15 15:04:31 +0100

    maj : editCtrl

--- WebContent/app/js/media/controller/editCtrl.js
+++ WebContent/app/js/media/controller/editCtrl.js
@@ -2,6 +2,8 @@
  * Controleur pour la modification d'un media
  */
 angular.module('media').controller('editCtrl', ['$scope', '$routeParams', 'mediaService', function($scope, $routeParams, mediaService) {
-	var media = getOne($routeParams.id);
-	mediaService.editMedia(media);
+	var media = $scope.media;
+	mediaService.editMedia(media).then(function(media) {
+		
+	});
 }])
